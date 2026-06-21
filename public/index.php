39614f503145d98aee222d1b1f4dec25964b986d
Andrii Fedorov <andrii.fedorov@infis.cz>
2026-06-21 22:53:09 +0200

    Refactor: streamline index.php to render Latte templates with product data and add new ProductSeeder for database seeding

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -7,21 +7,12 @@ use Latte\Engine;
 
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
-$stockManager = $container->getByType(App\Service\StockManager::class);
-$sku = 'SOCK-FUN-CAT';
 
-// 1. Имитируем, что мы загрузили 2 пары носков на склад
-$stockManager->syncStock($sku, 2);
+$productRepository = $container->getByType(App\Repository\ProductRepository::class);
+$products = $productRepository->getAllProducts();
 
-// 2. Покупатель 1 забирает 2 пары
-$attempt1 = $stockManager->reserve($sku, 2);
-
-// 3. Покупатель 2 пытается забрать еще 1 пару
-$attempt2 = $stockManager->reserve($sku, 1);
-
-echo "<h3>Тест Highload Резервирования</h3>";
-echo "Попытка 1 (Купить 2): " . ($attempt1 ? '<span style="color:green">УСПЕХ</span>' : 'ОТКАЗ') . "<br>";
-echo "Попытка 2 (Купить 1): " . ($attempt2 ? '<span style="color:green">УСПЕХ</span>' : '<span style="color:red">ОТКАЗ (Нет на складе)</span>') . "<br>";
-echo "Остаток в Redis: " . $stockManager->getCurrentStock($sku) . " шт.";
-
-exit; // Останавливаем выполнение, чтобы Latte пока не рисовал витрину
+$latte = new Engine();
+$latte->setCacheDirectory(__DIR__ . '/../temp');
+$latte->render(__DIR__ . '/../src/Templates/home.latte', [
+    'products' => $products
+]);
